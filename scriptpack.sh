3bc89864c24c76fcf9f6793f2f966c3aa163daae
Hector-Camacho <hector.camacho.f@gmail.com>
2018-02-19 20:23:03 -0600

    [ADD][aircrack] Se añadio el modulo aircrack junto con wifite para auditoria de redes informaticas

--- scriptpack.sh
+++ scriptpack.sh
@@ -1,45 +1,86 @@
 #!/bin/bash
-"#############################################"
-echo sigueme en mi canal de youtobe 3xploit 
-"##############################################"
-apt install figlet 
-figlet 3xploit
-apt install sl 
+VERDE='\033[0;32m'
+NC='\033[0m' # No Color
+
+apt install figlet
+
+echo "${VERDE}[+]${NC} Instalando git ${VERDE}[+][+][+][+][+][+][+]${NC}"
+apt install sl
 sl
-echo 3xploit 
-"####################"
+
+echo "${VERDE}[+]${NC} Instalando sl ${VERDE}[+][+][+][+][+][+][+]${NC}"
+apt install sl
+sl
+
 figlet nmap
-"#################"
-echo instalando nmap
+echo "${VERDE}[+]${NC} Instalando nmap ${VERDE}[+][+][+][+][+][+][+]${NC}"
 apt install nmap
-apt install python
+
 figlet python
-apt install php
+echo "${VERDE}[+]${NC} Instalando python,python requests y pip ${VERDE}[+][+][+][+][+][+][+]${NC}"
+apt install python
+apt install python-requests
+apt install python3-pip
+
+figlet aircrack-ng
+echo "${VERDE}[+]${NC} Instalando aircrack-ng${VERDE}[+][+][+][+][+][+][+]${NC}"
+apt install aircrack-ng
+
+figlet wifite
+echo "${VERDE}[+]${NC} Clonando wifite${VERDE}[+][+][+][+][+][+][+]${NC}"
+git clone https://github.com/derv82/wifite
+
+
+figlet python
+echo "${VERDE}[+]${NC} Instalando python, ${VERDE}[+][+][+][+][+][+][+]${NC}"
+
 figlet php
+echo "${VERDE}[+]${NC} Instalando php ${VERDE}[+][+][+][+][+][+][+]${NC}"
+apt install php
+
+echo "${VERDE}[+]${NC} Instalando python 2 ${VERDE}[+][+][+][+][+][+][+]${NC}"
 apt install python2
-"#################"
-echo instalando python2
-"###################"
-echo 3xploit termux suscribete a mi canal 
-figlet red_hawk
+
+echo "${VERDE}[+]${NC} Clonando red_hawk ${VERDE}[+][+][+][+][+][+][+]${NC}"
 git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+
 figlet credmap
+echo "${VERDE}[+]${NC} Clonando credmap ${VERDE}[+][+][+][+][+][+][+]${NC}"
 git clone https://github.com/lightos/credmap.git
+
 figlet sqlmap
+echo "${VERDE}[+]${NC} Clonando sqlmap ${VERDE}[+][+][+][+][+][+][+]${NC}"
 git clone https://github.com/sqlmapproject/sqlmap.git
+
 figlet recondog 
+echo "${VERDE}[+]${NC} Clonando recondog ${VERDE}[+][+][+][+][+][+][+]${NC}"
 git clone https://github.com/UltimateHackers/ReconDog.git
+
 figlet ngrok 
+echo "${VERDE}[+]${NC} Clonando ngrok ${VERDE}[+][+][+][+][+][+][+]${NC}"
 git clone https://github.com/inconshreveable/ngrok.git
+
+figlet tor 
+echo "${VERDE}[+]${NC} Instalando tor ${VERDE}[+][+][+][+][+][+][+]${NC}"
 apt install tor
-echo instalando tor
+
+figlet perl
+echo "${VERDE}[+]${NC} Instalando perl ${VERDE}[+][+][+][+][+][+][+]${NC}"
 apt install perl 
+
+figlet w3m
+echo "${VERDE}[+]${NC} Instalando w3m ${VERDE}[+][+][+][+][+][+][+]${NC}"
 apt install w3m 
-echo instalando w3m
+
+figlet apache
+echo "${VERDE}[+]${NC} Instalando apache ${VERDE}[+][+][+][+][+][+][+]${NC}"
 apt install apache2
+
+
 apt install cowsay
-cowsay 3xploit 
-figlet 3xploit
-"###########################"
-echo suscribete a mi canal
-"###########################"
+
+cowsay 3xploit
+echo suscribete al canal de 3xploit
+echo ${VERDE}[+]${NC} https://www.youtube.com/channel/UCW0SMRol_DXBd9rL-QEGOeg
+echo con una colaboracion de w4rm4ch1ne
+
