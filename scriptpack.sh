784648ed87b14545616d4b67c0bf6efd6d1b5a42
Hector-Camacho <hector.camacho.f@gmail.com>
2018-02-19 21:52:42 -0600

    [FIX] Python downgraded to 2.7 due to conflicts with some frameworks

--- scriptpack.sh
+++ scriptpack.sh
@@ -3,84 +3,71 @@ VERDE='\033[0;32m'
 NC='\033[0m' # No Color
 
 apt install figlet
+cd 
+echo "${VERDE}[+][+][+][+][${NC} Instalando git ${VERDE}[+][+][+][+]${NC}"
+apt install -y git
 
-echo "${VERDE}[+]${NC} Instalando git ${VERDE}[+][+][+][+][+][+][+]${NC}"
-apt install sl
-sl
-
-echo "${VERDE}[+]${NC} Instalando sl ${VERDE}[+][+][+][+][+][+][+]${NC}"
-apt install sl
+echo "${VERDE}[+][+][+][+][${NC} Instalando sl ${VERDE}[+][+][+][+]${NC}"
+apt install -y sl
 sl
 
 figlet nmap
-echo "${VERDE}[+]${NC} Instalando nmap ${VERDE}[+][+][+][+][+][+][+]${NC}"
-apt install nmap
-
-figlet python
-echo "${VERDE}[+]${NC} Instalando python,python requests y pip ${VERDE}[+][+][+][+][+][+][+]${NC}"
-apt install python
-apt install python-requests
-apt install python3-pip
+echo "${VERDE}[+][+][+][+][${NC} Instalando nmap ${VERDE}[+][+][+][+]${NC}"
+apt install -y nmap
 
 figlet aircrack-ng
-echo "${VERDE}[+]${NC} Instalando aircrack-ng${VERDE}[+][+][+][+][+][+][+]${NC}"
-apt install aircrack-ng
+echo "${VERDE}[+][+][+][+][${NC} Instalando aircrack-ng${VERDE}[+][+][+][+]${NC}"
+apt install -y aircrack-ng
 
 figlet wifite
-echo "${VERDE}[+]${NC} Clonando wifite${VERDE}[+][+][+][+][+][+][+]${NC}"
+echo "${VERDE}[+][+][+][+][${NC} Clonando wifite${VERDE}[+][+][+][+]${NC}"
 git clone https://github.com/derv82/wifite
 
+figlet php
+echo "${VERDE}[+][+][+][+][${NC} Instalando php ${VERDE}[+][+][+][+]${NC}"
+apt install -y php
 
 figlet python
-echo "${VERDE}[+]${NC} Instalando python, ${VERDE}[+][+][+][+][+][+][+]${NC}"
+echo "${VERDE}[+][+][+][+][${NC} Instalando python 2 ${VERDE}[+][+][+][+]${NC}"
+apt install -y python2
 
-figlet php
-echo "${VERDE}[+]${NC} Instalando php ${VERDE}[+][+][+][+][+][+][+]${NC}"
-apt install php
-
-echo "${VERDE}[+]${NC} Instalando python 2 ${VERDE}[+][+][+][+][+][+][+]${NC}"
-apt install python2
-
-echo "${VERDE}[+]${NC} Clonando red_hawk ${VERDE}[+][+][+][+][+][+][+]${NC}"
+echo "${VERDE}[+][+][+][+][${NC} Clonando red_hawk ${VERDE}[+][+][+][+]${NC}"
 git clone https://github.com/Tuhinshubhra/RED_HAWK.git
 
 figlet credmap
-echo "${VERDE}[+]${NC} Clonando credmap ${VERDE}[+][+][+][+][+][+][+]${NC}"
+echo "${VERDE}[+][+][+][+][${NC} Clonando credmap ${VERDE}[+][+][+][+]${NC}"
 git clone https://github.com/lightos/credmap.git
 
 figlet sqlmap
-echo "${VERDE}[+]${NC} Clonando sqlmap ${VERDE}[+][+][+][+][+][+][+]${NC}"
+echo "${VERDE}[+][+][+][+][${NC} Clonando sqlmap ${VERDE}[+][+][+][+]${NC}"
 git clone https://github.com/sqlmapproject/sqlmap.git
 
 figlet recondog 
-echo "${VERDE}[+]${NC} Clonando recondog ${VERDE}[+][+][+][+][+][+][+]${NC}"
+echo "${VERDE}[+][+][+][+][${NC} Clonando recondog ${VERDE}[+][+][+][+]${NC}"
 git clone https://github.com/UltimateHackers/ReconDog.git
 
 figlet ngrok 
-echo "${VERDE}[+]${NC} Clonando ngrok ${VERDE}[+][+][+][+][+][+][+]${NC}"
+echo "${VERDE}[+][+][+][+][${NC} Clonando ngrok ${VERDE}[+][+][+][+]${NC}"
 git clone https://github.com/inconshreveable/ngrok.git
 
 figlet tor 
-echo "${VERDE}[+]${NC} Instalando tor ${VERDE}[+][+][+][+][+][+][+]${NC}"
-apt install tor
+echo "${VERDE}[+][+][+][+][${NC} Instalando tor ${VERDE}[+][+][+][+]${NC}"
+apt install -y tor
 
 figlet perl
-echo "${VERDE}[+]${NC} Instalando perl ${VERDE}[+][+][+][+][+][+][+]${NC}"
-apt install perl 
+echo "${VERDE}[+][+][+][+][${NC} Instalando perl ${VERDE}[+][+][+][+]${NC}"
+apt install -y perl 
 
 figlet w3m
-echo "${VERDE}[+]${NC} Instalando w3m ${VERDE}[+][+][+][+][+][+][+]${NC}"
-apt install w3m 
+echo "${VERDE}[+][+][+][+][${NC} Instalando w3m ${VERDE}[+][+][+][+]${NC}"
+apt install -y w3m 
 
 figlet apache
-echo "${VERDE}[+]${NC} Instalando apache ${VERDE}[+][+][+][+][+][+][+]${NC}"
-apt install apache2
+echo "${VERDE}[+][+][+][+][${NC} Instalando apache ${VERDE}[+][+][+][+]${NC}"
+apt install -y apache2
+apt install -y cowsay
 
-
-apt install cowsay
-
-cowsay 3xploit
-echo suscribete al canal de 3xploit
-echo ${VERDE}[+]${NC} https://www.youtube.com/channel/UCW0SMRol_DXBd9rL-QEGOeg
-echo con una colaboracion de w4rm4ch1ne
-
+cowsay 3xploit - w4rm4ch1ne
+echo suscribete al canal de 3xploit para saber mas del tema
+echo ${VERDE}[+] https://www.youtube.com/channel/UCW0SMRol_DXBd9rL-QEGOeg ${NC} 
+echo codificado por: ${VERDE}3xploit${NC} y ${VERDE}w4rm4ch1ne${NC} 
